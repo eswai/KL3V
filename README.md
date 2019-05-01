4f1152420f688993224356c395a5c0bce52a623a
eswai <>
2019-05-01 16:47:26 +0200

    Keytop legend texture

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Keyboard layout 3D viewer is based on three.js and WebGL.
 https://eswai.github.io/KL3V/kl3v.html
 
 SA and Cherry keycaps are Copyright (c) 2017 Josh Hinnebusch. 
+Cherry MX switch (c) Mirko Tadic.
--- js/KLEparser.js
+++ js/KLEparser.js
@@ -1,5 +1,6 @@
 class Key {
-  constructor(x, y, r, w, h, p, c) {
+  constructor(kc, x, y, r, w, h, p, c, t, f, f2) {
+    this.kc = kc
     this.x = x
     this.y = y
     this.r = r
@@ -7,6 +8,9 @@ class Key {
     this.h = h
     this.p = p
     this.c = c
+    this.t = t
+    this.f = f
+    this.f2 = f2
   }
 
   centerX() {
@@ -32,6 +36,14 @@ class Key {
   scaleH() {
     return this.h * 19 / 18.5 - 1 / 18.5
   }
+
+  cInt() {
+    parseInt(this.c.replace('#', '0x'))
+  }
+
+  tInt() {
+    parseInt(this.t.replace('#', '0x'))
+  }
 }
 
 function KLEparser(raw) {
@@ -39,6 +51,7 @@ function KLEparser(raw) {
   console.log(json)
   var layout = JSON.parse(json)
 
+  var kc = "Q"
   var ax = 0
   var ay = 0
   var w = 1
@@ -50,6 +63,9 @@ function KLEparser(raw) {
   var dy = 0
   var p = "DSA"
   var c = 0xcfd8dc
+  var t = 0xffffff
+  var f = 3
+  var f2 = 3
   var keys = []
   var keyboard = {keys: keys}
 
@@ -57,10 +73,11 @@ function KLEparser(raw) {
     if (Array.isArray(row)) {
       row.forEach(function(key){
         if (typeof key == 'string') {
+          var kc = key
           var px = (rx + Math.cos(r) * ax - Math.sin(r) * ay)
           var py = (ry + Math.sin(r) * ax + Math.cos(r) * ay)
 
-          keys.push(new Key(px, py, r, w, h, p, c))
+          keys.push(new Key(kc, px, py, r, w, h, p, c, t, f, f2))
 
           ax = ax + w
           w = 1
@@ -87,7 +104,10 @@ function KLEparser(raw) {
             ay = ay + dy
           }
           p = key.p ? key.p : p
-          c = key.c ? parseInt(key.c.replace('#', '0x')) : c
+          c = key.c ? key.c : c
+          t = key.t ? key.t : t
+          f = key.f ? parseInt(key.f) : f
+          f2 = key.f2 ? parseInt(key.f2) : f2
           r = "r" in key ? key.r / 180 * Math.PI : r
         }
       })
